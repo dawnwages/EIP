233a0d793a784302e3e6a8b038c628fc1e97b3ad
Dawn Wages <dwages@Lenovo.com>
2017-05-17 15:52:48 -0400

    fixed going to fast. post_detail not configured yet

--- CMS/views.py
+++ CMS/views.py
@@ -10,7 +10,7 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'CMS/post_list.html', {'posts': posts})
 
 def simple_upload(request):
     if request.method == 'POST':
