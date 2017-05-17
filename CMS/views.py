2216571407c3e48bb275c7006d97058a0ca3f630
Dawn Wages <dwages@Lenovo.com>
2017-05-17 15:41:18 -0400

    fixing post in post_list.html

--- CMS/views.py
+++ CMS/views.py
@@ -8,7 +8,8 @@ from .forms import UploadFileForm
 from .models import Post
 
 def post_list(request):
-	return render(request, 'CMS/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def simple_upload(request):
     if request.method == 'POST':
