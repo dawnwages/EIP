18b7cc8dfa388cb142574fb3ada81ec53a518647
Dawn Wages <dwages@Lenovo.com>
2017-05-17 16:23:50 -0400

    added post_detail

--- CMS/urls.py
+++ CMS/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =[
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/new/$', views.simple_upload, name='post_new'),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 	]
--- CMS/views.py
+++ CMS/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from django.utils import timezone
 #from django.CMS.files.storage import FileSystemStorage
@@ -11,6 +11,12 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'CMS/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'CMS/post_detail.html', {'post': post})
+
 
 def simple_upload(request):
     if request.method == 'POST':
@@ -22,3 +28,4 @@ def simple_upload(request):
     else:
         form = UploadFileForm()
     return render(request, 'CMS/simple_upload.html', {'form': form})
+
