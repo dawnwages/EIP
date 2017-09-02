bc7003427333b4aa02cdd4f7050ad066df80f9cd
Dawn Wages <dwages@Lenovo.com>
2017-09-02 08:50:31 -0400

    updated the homepage, linklist and calendar. Need to work on registration, users and login

--- CMS/urls.py
+++ CMS/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns =[
+	url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
