45b668a45c332340d4699740031e828005ed43d8
Dawn Wages <dwages@Lenovo.com>
2017-09-07 20:19:29 -0400

    started profiles app

--- CMS/urls.py
+++ CMS/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns =[
 	url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^profile/', views.profile, name='profile page')
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
