b88d495ea238fb8b73c12412d2754a0e4a3ccc14
Dawn Wages <dwages@Lenovo.com>
2017-09-13 23:03:54 -0400

    I am a fucking boss and I'm making it look fucking purty

--- CMS/models.py
+++ CMS/models.py
@@ -10,16 +10,16 @@ class Post(models.Model):
 		title = models.CharField(max_length=200)
 		email = models.CharField(max_length=200)
 		url = models.URLField(max_length=255, blank=True) #RFC 3986 states there is no limit, but the hostname is limited to 255 characters because of DNS limitations.
-		Update_1 = '/static/img/update_1.jpg'
-		Update_2 = '/static/img/update_2.jpg'
-		Update_3 = '/static/img/update_3.jpg'
-		Update_4 = '/static/img/update_4.jpg'
-		Update_5 = '/static/img/update_5.jpg'
+		Update_1 = '/static/img/update_1.png'
+		Update_2 = '/static/img/update_2.png'
+		Update_3 = '/static/img/update_3.png'
+		Update_4 = '/static/img/update_4.png'
+		Update_5 = '/static/img/update_5.png'
 		Update_Types = (
 			(Update_1, 'Promotions'),
 			(Update_2, 'Special Projects'),
 			(Update_3, 'Campaigns'),
-			(Update_4, 'Holidays'),
+			(Update_4, 'Operations'),
 			(Update_5, 'Tests')
 			)
 		Update_Choices = models.CharField(
@@ -28,7 +28,7 @@ class Post(models.Model):
 			default=Update_2,
 			)
 		text = models.TextField(blank=True)
-		asset = models.FileField(upload_to='post_assets/%Y/%m/',blank=True)
+		asset = models.FileField(upload_to='post_assets/%Y/%m/', blank=True,)
 		created_date = models.DateTimeField(
 			default=timezone.now)
 		published_date = models.DateTimeField(
--- CMS/views.py
+++ CMS/views.py
@@ -10,6 +10,7 @@ from django.utils import timezone
 #from django.CMS.files.storage import FileSystemStorage
 from .forms import PostForm
 from .models import Post, Link
+from profiles.models import Profile
 
 def link_list(request):
     links = Link.objects.all()
@@ -20,16 +21,20 @@ def calendar(request):
 
 def post_list(request):
     
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     #return render(request, 'CMS/post_list.html', {'posts': posts})
 
     return render(request, 'CMS/post_list.html', {'posts': posts})
 
 def my_view(request):
+    avitar = None
     username = None
     if request.user.is_authenticated():
         username = request.user.username
+        avitar = request.Profile.avitar
     return render(request, 'CMS/base.html', username)
+    return render(request, 'CMS/base.html', avitar)
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
